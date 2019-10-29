71febb5031f9423bf8350558ae493b5c324fc105
Zoya <zoyakrasnopolskaa@gmail.com>
2019-10-29 10:52:12 +0200

    column editing

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ $('.slider').slick({
     infinite: false,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 2,
     arrows: false,
+    autoplay: true,
+   autoplaySpeed: 2000,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 2,
           infinite: true,
           dots: true
         }
