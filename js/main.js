ea53d918af5b34a03c449bde1f22769fe1449a8c
Zoya <zoyakrasnopolskaa@gmail.com>
2019-11-06 22:15:32 +0200

    changes on the first page, changed tabs and banner

--- js/main.js
+++ js/main.js
@@ -1,44 +1,45 @@
 $(function() {
-  $( "#tabs" ).tabs();
+  $( ".tabs" ).tabs();
   $( ".product-details-tab-area" ).tabs();
-  $('.dropdown-toggle').dropdown();
+  //$('.dropdown-toggle').dropdown();
   $('.slider').slick({
-      dots: true,
-      infinite: false,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 2,
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
         }
-        
-      ]
-    })
-  });
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  })
+});
+
  
 
 
